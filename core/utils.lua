9bd34b2f4cc4303b8702dd397c09165baf7059e8
sigmacodeslol <m96153045@gmail.com>
2025-06-29 10:48:08 +0100

    Update utils.lua

--- core/utils.lua
+++ core/utils.lua
@@ -1,8 +1,12 @@
 utilslib = {}
 
-function utilslib.loadgithub(http, isTrusted)
+function utilslib.loadgithub(file, isTrusted, repo_loc) -- repo_loc is repository location
     isTrusted = isTrusted or false
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/"..http), isTrusted)()
+    repo_loc = repo_loc or false
+    if repo_loc == false then
+        repo_loc = "sigmacodeslol/myscripts/blob/master/"
+    end
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/"..repo_loc..file), isTrusted)()
 end
 
 return utilslib
