180d72fc1d67a0fec12e1d2bff5d6f1755ed4e93
sigmacodeslol <m96153045@gmail.com>
2025-06-29 10:42:22 +0100

    Update utils.lua

--- core/utils.lua
+++ core/utils.lua
@@ -1,7 +1,8 @@
-GG = (getgenv and getgenv()) or _G or shared or false;
-GG.GG = GG;
+utilslib = {}
 
-function GG.loadgithub(http, isTrusted)
+function utilslib.loadgithub(http, isTrusted)
     isTrusted = isTrusted or false
     return loadstring(game:HttpGet("https://raw.githubusercontent.com/"..http), isTrusted)()
 end
+
+return utilslib
